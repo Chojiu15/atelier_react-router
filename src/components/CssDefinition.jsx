e94dd3c7494d109d71eb8a99fcffe1f6e05d8214
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-15 16:59:45 +0200

    adding css template

--- src/components/CssDefinition.jsx
+++ src/components/CssDefinition.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const CssDefinition = () => {
-	return <div>css</div>;
+	const stylus = {
+		textAlign: 'center'
+	};
+	return (
+		<div style={stylus}>
+			<h1>Le CSS</h1>
+			<p>
+				Le terme CSS est l'acronyme anglais de Cascading Style Sheets qui peut se traduire par "feuilles de style en
+				cascade". Le CSS est un langage informatique utilisé sur l'internet pour mettre en forme les fichiers HTML ou
+				XML. Ainsi, les feuilles de style, aussi appelé les fichiers CSS, comprennent du code qui permet de gérer le
+				design d'une page en HTML.
+			</p>
+		</div>
+	);
 };
 
 export default CssDefinition;
