86cff1f9b8b62b260c19d37607ed88bf89db80ba
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-04-15 17:00:26 +0200

    adding javascript template

--- src/components/JsDefinition.jsx
+++ src/components/JsDefinition.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const JsDefinition = () => {
-	return <div>js</div>;
+	const stylus = {
+		textAlign: 'center'
+	};
+	return (
+		<div style={stylus}>
+			<h1>Le Javascript</h1>
+			<p>
+				Le JavaScript est un langage informatique utilisé sur les pages web. Ce langage à la particularité de s'activer
+				sur le poste client, en d'autres mots c'est votre ordinateur qui va recevoir le code et qui devra l'exécuter.
+				C'est en opposition à d'autres langages qui sont activé côté serveur. L'exécution du code est effectué par votre
+				navigateur internet tel que Firefox ou Internet Explorer.
+			</p>
+		</div>
+	);
 };
 
 export default JsDefinition;
